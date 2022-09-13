a76be0c386362ee482e8f16bc3d276b88251feff
Tsiory Rarison <mendritianararison@gmail.com>
2022-09-13 19:06:51 +0200

    Ajout De texte

---
+++ index.java
@@ -0,0 +1,9 @@
+public class index {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Voici notre Premier Projet Simple");
+        System.out.println();
+
+    }
+}
