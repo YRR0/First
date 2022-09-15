9c466dbd68a95f067799ce1d3ce4a25405e7bf32
Tsiory Rarison <mendritianararison@gmail.com>
2022-09-15 09:13:23 +0200

    Ajout methode Enum

--- index.java
+++ index.java
@@ -5,5 +5,19 @@ public class index {
         System.out.println("Voici notre Premier Projet Simple");
         System.out.println();
 
+        System.out.println("Ajout de notre methode enum");
+        enumMot("BIEN");
+
+    }
+
+    public static void enumMot(
+
+            String a) {
+        int l = a.length();
+        int i = 0;
+        while (i < l) {
+            System.out.println(a);
+            i++;
+        }
     }
 }
